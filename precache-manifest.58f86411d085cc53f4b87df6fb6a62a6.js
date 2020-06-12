6f1c40503a655697f4a1d91784ea668f72ed9f1d
AlArafRabby <alrabbyjoyprince5@gmail.com>
2020-06-12 23:29:00 +0600

    Updates

---
+++ precache-manifest.58f86411d085cc53f4b87df6fb6a62a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0036c1fbb7f6733b244d21fc4e6e0511",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "9248712d0308253479ff",
+    "url": "/React-Deploy/static/css/2.5206c547.chunk.css"
+  },
+  {
+    "revision": "3a379c1077e1f2d1a9ab",
+    "url": "/React-Deploy/static/css/main.8508aeb2.chunk.css"
+  },
+  {
+    "revision": "9248712d0308253479ff",
+    "url": "/React-Deploy/static/js/2.01d2e34c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Deploy/static/js/2.01d2e34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a379c1077e1f2d1a9ab",
+    "url": "/React-Deploy/static/js/main.fefa7933.chunk.js"
+  },
+  {
+    "revision": "87da3cf206756b19eefa",
+    "url": "/React-Deploy/static/js/runtime-main.ad364426.js"
+  }
+]);
